2d558315e6850cae0750965a6a8b25570b2a9b16
Gunter <xc200925@gmail.com>
2021-08-17 10:13:34 +0900

    SettingViewController 에 아울렛 변수와 액션 함수 추가
    액션 함수 추가

--- LEDBoard/SettingViewController.swift
+++ LEDBoard/SettingViewController.swift
@@ -8,7 +8,25 @@
 import UIKit
 
 class SettingViewController: UIViewController {
+
+  @IBOutlet weak var textField: UITextField!
+
+  @IBOutlet weak var yellowButton: UIButton!
+  @IBOutlet weak var purpleButton: UIButton!
+  @IBOutlet weak var greenButton: UIButton!
+
+  @IBOutlet weak var blackButton: UIButton!
+  @IBOutlet weak var blueButton: UIButton!
+  @IBOutlet weak var orangeButton: UIButton!
+
   override func viewDidLoad() {
     super.viewDidLoad()
+  }
+
+  @IBAction func tapTextColorButton(_ sender: UIButton) {
+  }
+  @IBAction func tapBackgroundColorButton(_ sender: UIButton) {
+  }
+  @IBAction func tapSaveButton(_ sender: Any) {
   }
 }
