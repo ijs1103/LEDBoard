e5ea978ffcaebd34a054f80706e01a5f629fe987
Gunter <xc200925@gmail.com>
2021-08-17 10:49:14 +0900

    ViewController 에서 delegate 위임 받고 LEDBoardSettingDelegate 프로토콜 채택후에 준수

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
+
   @IBOutlet weak var contentsLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
     self.contentsLabel.textColor = .yellow
   }
+
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if let settingViewController = segue.destination as? SettingViewController {
+      settingViewController.delegate = self
+    }
+  }
+
+  func changedSetting(text: String?, textColor: UIColor, backgroudColor: UIColor) {
+    if let text = text {
+      self.contentsLabel.text = text
+    }
+    self.contentsLabel.textColor = textColor
+    self.view.backgroundColor = backgroudColor
+  }
 }
+
 
