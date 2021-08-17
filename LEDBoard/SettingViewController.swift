556782424c9fe5c702169944b055640b3ce4b514
Gunter <xc200925@gmail.com>
2021-08-17 10:10:54 +0900

    빈 SettingViewController 추가

---
+++ LEDBoard/SettingViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SettingViewController.swift
+//  LEDBoard
+//
+//  Created by Gunter on 2021/08/17.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
